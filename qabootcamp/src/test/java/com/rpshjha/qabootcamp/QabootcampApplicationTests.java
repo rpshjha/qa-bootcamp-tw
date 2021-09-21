24bcfe604c29d80fd6d764508dbfe04a27be645a
Rupesh Kumar <rupeshkumar@Rupeshs-MacBook-Pro.local>
2021-09-21 11:52:05 +0530

    added testng.xml

--- qabootcamp/src/test/java/com/rpshjha/qabootcamp/QabootcampApplicationTests.java
+++ qabootcamp/src/test/java/com/rpshjha/qabootcamp/QabootcampApplicationTests.java
@@ -1,13 +1,33 @@
 package com.rpshjha.qabootcamp;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
 
 @SpringBootTest
 class QabootcampApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+
+    private WebDriver webDriver;
+
+    @BeforeTest
+    public void setup() {
+        WebDriverManager.chromedriver().setup();
+        webDriver = new ChromeDriver();
+        webDriver.get("http://localhost:3000/#/");
+    }
+
+    @Test
+    void contextLoads() {
+    }
+
+    @AfterTest
+    public void tear() {
+        webDriver.quit();
+    }
 
 }
