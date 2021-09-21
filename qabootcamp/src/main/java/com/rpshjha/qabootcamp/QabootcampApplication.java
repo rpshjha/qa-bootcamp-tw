72d033a9fd904a9fbfbdff88709653fc0d1afd2a
Rupesh Kumar <rupeshkumar@Rupeshs-MacBook-Pro.local>
2021-09-21 11:18:30 +0530

    initial commit

---
+++ qabootcamp/src/main/java/com/rpshjha/qabootcamp/QabootcampApplication.java
@@ -0,0 +1,13 @@
+package com.rpshjha.qabootcamp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QabootcampApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QabootcampApplication.class, args);
+	}
+
+}
